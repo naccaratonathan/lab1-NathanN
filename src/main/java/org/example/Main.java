20a88edc42ddb3af8bff9bf443e6579be1df143e
Nathan <naccaratonathan@gmail.com>
2023-02-23 10:04:14 -0500

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,22 +2,25 @@ package org.example;
 
 import javax.swing.*;
 import java.util.Scanner;
-
+/*
+ Question 1, this would fail to run as the java file and class file have to be the same name
+ Question 2, The main method must be declared as public static void so that the JVM is able to access the class.
+ Question 3, Variables must be declared as a type as it determines the values inside and the actions performed.
+*/
 public class Main {
-    public static void main(String[] args) {
-        getNameFromGui();
+    private static String getNameFromGui() {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        System.out.println(name);
+        return name;
+
     }
-
-        /*
-         Question 1, this would fail to run as the java file and class file have to be the same name
-         Question 2, The main method must be declared as public static void so that the JVM is able to access the class.
-         Question 3, Variables must be declared as a type as it determines the values inside and the actions performed.
-        */
-        private static void getNameFromGui() {
+    public static void printName(String name) {System.out.println("Hello, "+name+"!");}
             // This can be marked as private because it is being used within the same class.
-            String name = JOptionPane.showInputDialog("What is your name?");
-            System.out.println(name);
-            System.out.println("Hello " +name);
 
+
+
+    public static void main(String[] args) {
+            String name = getNameFromGui();
+            printName(name);
     }
 }
