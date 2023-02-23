bccdebba4a2a8e9ab4aa9cedebd58ee2d92c44df
Nathan <naccaratonathan@gmail.com>
2023-02-23 10:59:01 -0500

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -15,9 +15,6 @@ public class Main {
     }
     public static void printName(String name) {System.out.println("Hello, "+name+"!");}
 
-
-
-
     public static void main(String[] args) {
             String name = getNameFromGui();
             printName(name);
